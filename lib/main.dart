2fb802368eb20bc3928e0cb2aedbb511c867fd23
its-faatimas <yourgitemail@example.com>
2022-06-02 12:21:50 +0500

    hive is initializing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:cat_api/bloc/cats_bloc_view.dart';
+import 'package:cat_api/screens/cats_bloc_view.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
 
   runApp(const MyApp());
